53793cfb66ba5581f9bc2b55a640cef2eb916740
corey-smith1 <corey_smith661@hotmail.com>
2017-08-01 12:23:55 +1000

    added first task

---
+++ TableAndChairs/src/TabsandChars.java
@@ -0,0 +1,17 @@
+/**
+ * Created by jc427701 on 1/08/17.
+ */
+public class TabsandChars {
+
+    public static void main(String[] args)
+    {
+        System.out.println("x                    x");
+        System.out.println("x                    x");
+        System.out.println("x     xxxxxxxxxx     x");
+        System.out.println("xxxxx x        x xxxxx");
+        System.out.println("x   x x        x x   x");
+        System.out.println("x   x x        x x   x");
+
+    }
+}
+
