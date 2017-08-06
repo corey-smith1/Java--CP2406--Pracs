0d8c8ac9fea6276119b6f24dcff7753b55ba6acc
corey-smith1 <corey_smith661@hotmail.com>
2017-08-06 22:16:23 +1000

    Complete task 1

---
+++ Prac_1/src/MovieQuote.java
@@ -0,0 +1,13 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+public class MovieQuote {
+    public MovieQuote() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There's to many people on this earth.... we need a new plague");
+    }
+}
